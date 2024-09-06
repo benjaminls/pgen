f6a3d575666d048ad7db936f29ee05d624ff801f
Benjamin Lawrence-Sanderson <82254480+benjaminls@users.noreply.github.com>
2024-09-06 03:52:07 -0500

    Add spdlog and debug preprocessor directive

--- main.cpp
+++ main.cpp
@@ -5,28 +5,34 @@
 #include <stdlib.h>
 #include <argparse/argparse.hpp>
 
+#include "spdlog/spdlog.h"
 #include "include/wordhist.hpp"
+
+#define DEBUG true
 
 int main( int argc, char *argv[] ) {
     std::string filename = "lists/nouns.txt";
 
     std::vector<std::string> words;
-
+    
     std::ifstream file(filename);
-
+    
     if (!file) {
         std::cerr << "Error: Unable to open file " << filename << std::endl;
         return 1;
     }
-
+    
     // Read the file line by line and store each word in the vector
     std::string word;
     while (std::getline(file, word)) {
         words.push_back(word);
     }
-
+    
     // Close the file
     file.close();
+    
+    // Set the environment variable PGEN_DEBUG to the value of DEBUG
+    setenv("PGEN_DEBUG", DEBUG ? "1" : "0", 1);
 
     std::vector<int> wordLengths;
     createWordLengthVector(words, wordLengths);
