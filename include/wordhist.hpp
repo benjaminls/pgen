d98a0e372127785f3d10d65815b9face370f57e1
Benjamin Lawrence-Sanderson <82254480+benjaminls@users.noreply.github.com>
2024-09-06 03:52:42 -0500

    Redo histogram printing, change debug to preprocessor directive

--- include/wordhist.hpp
+++ include/wordhist.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 
-const int DEBUG = true;
+// #define DEBUG true
 
 void createWordLengthVector(const std::vector<std::string> &words, std::vector<int> &wordLengths){
     // Fill the wordLengths vector with the lengths of the words
@@ -68,31 +68,67 @@ void validateWordLengths(const std::vector<int> &wordLengths) {
 }
 
 /* Print histogram of word lengths */
-void printWordHist(const std::vector<int> &wordLengths, int numStars = 50, char starChar = '*') {    
+void printWordHist(const std::vector<int> &wordLengths, int numStars = 200, char starChar = '*') {    
     // Validate inputs
     validateNumStars(numStars);
     validateWordLengths(wordLengths);
     validateStarChar(starChar);
 
-    int maxLen = *std::max_element(wordLengths.begin(), wordLengths.end());
+    int maxWordLen = *std::max_element(wordLengths.begin(), wordLengths.end()); // Length of the longest word
+    int maxNumberLen = std::to_string(maxWordLen).length(); // Number of digits in the max length
+    std::vector<std::string> bars; // Vector to store the bars
+    std::vector<int> barLengths; // Vector to store the lengths of the bars
+    std::vector<std::string> linesOut(maxWordLen); // Vector to store the printed lines out
 
-    int maxNumberLen = std::to_string(maxLen).length(); // Number of digits in the max length
+    #ifdef DEBUG
+    std::cout << "Max word length: " << maxWordLen << std::endl;
+    std::cout << "Number of stars: " << numStars << std::endl;
+    std::cout << "Star character: " << starChar << std::endl;
+    std::cout << "Max length of axes numbers: " << maxNumberLen<< std::endl;
+    #endif
 
-    if (DEBUG) {
-        std::cout << "Max word length: " << maxLen << std::endl;
-        std::cout << "Number of stars: " << numStars << std::endl;
-        std::cout << "Star character: " << starChar << std::endl;
-        std::cout << "Max length of axes numbers: " << maxNumberLen<< std::endl;
+    // Fill bars vector with starChars
+    for (int i = 1; i <= maxWordLen; i++) {
+        int count = std::count(wordLengths.begin(), wordLengths.end(), i); // Count number of words of length i
+        int numStarsToPrint = (int) (numStars * ((double) count / wordLengths.size())); // Number of stars to print
+        barLengths.push_back(numStarsToPrint);
     }
 
-    for (int i = 1; i <= maxLen; i++) {
-        
-        std::cout << addBlankPrefixToNumber(i, maxNumberLen) << ": ";
-        int count = std::count(wordLengths.begin(), wordLengths.end(), i);
-        int numStarsToPrint = (int) (numStars * ((double) count / wordLengths.size()));
-        for (int j = 0; j < numStarsToPrint; j++) {
-            std::cout << starChar;
+    // Find the maximum bar length
+    int maxBarLen = *std::max_element(barLengths.begin(), barLengths.end());
+
+    const std::string leftAxis = ": "; // Axes string ": "
+    const std::string rightAxis = "| "; // Right border "| "
+    const int rightPadding = 2; // Right padding between bars and number of words
+
+    #ifdef DEBUG
+    std::cout << "Number of bars: " << barLengths.size() << std::endl;
+    std::cout << "Max bar length: " << maxBarLen << std::endl;
+    #endif 
+
+    // Fill the bars vector with the bars
+    for (int i = 0; i < maxWordLen; i++) {
+        std::string bar = "";
+        for (int j = 0; j < barLengths[i]; j++) {
+            bar += starChar;
         }
-        std::cout << std::endl;
+        bars.push_back(bar);
+    }
+
+    // Build the lines out to print
+    for (int i = 1; i <= maxWordLen; i++) {
+        linesOut[i - 1] = addBlankPrefixToNumber(i, maxNumberLen) + leftAxis.c_str() + bars[i - 1];
+        for (int j = 0; j < maxBarLen - barLengths[i - 1]; j++) {
+            linesOut[i - 1] += " ";
+        }
+        for (int j = 0; j < rightPadding; j++) {
+            linesOut[i - 1] += " ";
+        }
+        linesOut[i - 1] += rightAxis + std::to_string(std::count(wordLengths.begin(), wordLengths.end(), i));
+    }
+
+    // Print the histogram
+    for (std::string line : linesOut) {
+        std::cout << line << std::endl;
     }
 }
